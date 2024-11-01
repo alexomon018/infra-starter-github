1aa00f9d92b0bdeb75b4df57c847588dd527d92d
Aleksa <a.mitic@vegait.rs>
2024-11-01 11:15:53 +0100

    feat: added aws repos

--- locals.tf
+++ locals.tf
@@ -15,5 +15,21 @@ locals {
       topics             = ["frontend-masters", "terraform"]
       visibility         = "public"
     }
+
+    "infra-starter-aws-network" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "infra-starter-aws-network"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+
+    "infra-starter-aws-cluster" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "infra-starter-aws-cluster"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
   }
 }
